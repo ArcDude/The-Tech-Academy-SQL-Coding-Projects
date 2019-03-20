87be392ea57161c6a33890ef1c4f54ed8796214c
Blakeschu <47337463+ArcDude@users.noreply.github.com>
2019-03-19 22:22:16 -0500

    Update Tech_Acadamy_Final.sql

--- Tech_Academy_Final.sql
+++ Tech_Academy_Final.sql
@@ -1,5 +1,10 @@
-USE master -- For some reason this is the only database the queries work in, I'm not sure when or how, but the data
-			-- from db_library3 (the original database) was messed up at some point
+USE master
+GO
+DROP DATABASE db_library3
+GO
+CREATE DATABASE db_library3
+GO
+USE db_library3
 
 IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES)
 	DROP TABLE library_Branch, publisher, books, borrower, book_Copies, book_Loans, book_Authors
@@ -23,8 +28,6 @@ CREATE TABLE books (
 	book_title VARCHAR(70)  NOT NULL,
 	book_publisher VARCHAR(50) NOT NULL CONSTRAINT fk_publisher_name FOREIGN KEY REFERENCES publisher(publisher_name)
 );
-ALTER TABLE books 
-ALTER COLUMN book_title VARCHAR(70) NOT NULL;
 
 CREATE TABLE borrower (
 	card_no INT PRIMARY KEY NOT NULL IDENTITY (1000,1),
@@ -39,8 +42,9 @@ CREATE TABLE book_Copies (
 	number_of_copies INT NOT NULL 
 );
 
+
 CREATE TABLE book_Loans (
-	loans_book_id INT  NOT NULL CONSTRAINT fk_book_id2 FOREIGN KEY REFERENCES books(book_id),
+	loans_book_id INT NOT NULL CONSTRAINT fk_book_id2 FOREIGN KEY REFERENCES books(book_id),
 	loans_branch_id INT NOT NULL CONSTRAINT fk_branch_id2 FOREIGN KEY REFERENCES library_Branch(branch_id),
 	loans_card_no INT NOT NULL CONSTRAINT fk_card_no FOREIGN KEY REFERENCES borrower(card_no),
 	date_out VARCHAR(50) NOT NULL,
@@ -128,46 +132,46 @@ INSERT INTO book_Copies
 		Refer to Table: books for book_id*/
 	(copies_book_id, copies_branch_id, number_of_copies)
 	VALUES
-	(101, 1, 2), --1,1 First number = branch_id, Second number = number of books in that branch
-	(110, 1, 2), --1,2
-	(111, 1, 2), --1,3
-	(112, 1, 2), --1,4
-	(113, 1, 2), --1,5
-	(114, 1, 2), --1,6
-	(115, 1, 2), --1,7
-	(116, 1, 2), --1,8
-	(117, 1, 2), --1,9
-	(118, 1, 2), --1,10
-	(102, 2, 2), --2,1
-	(103, 2, 2), --2,2
-	(104, 2, 2), --2,3
-	(105, 2, 2), --2,4
-	(119, 2, 2), --2,5
-	(120, 2, 2), --2,6
-	(121, 2, 2), --2,7
-	(101, 2, 2), --2,8
-	(110, 2, 2), --2,9
-	(111, 2, 2), --2,10
-	(106, 3, 2), --3,1
-	(107, 3, 2), --3,2
-	(108, 3, 2), --3,3
-	(109, 3, 2), --3,4
-	(120, 3, 2), --3,5
-	(121, 3, 2), --3,6
-	(101, 3, 2), --3,7
-	(103, 3, 2), --3,8
-	(104, 3, 2), --3,9
-	(105, 3, 2), --3,10
-	(106, 4, 2), --4,1
-	(107, 4, 2), --4,2
-	(108, 4, 2), --4,3
-	(109, 4, 2), --4,4
-	(102, 4, 2), --4,5
-	(103, 4, 2), --4,6
-	(110, 4, 2), --4,7
-	(111, 4, 2), --4,8
-	(112, 4, 2), --4,9
-	(113, 4, 2) --4,10
+	(100, 1, 2), --1,1 First number = branch_id, Second number = number of books in that branch
+	(109, 1, 2), --1,2
+	(110, 1, 2), --1,3
+	(111, 1, 2), --1,4
+	(112, 1, 2), --1,5
+	(113, 1, 2), --1,6
+	(114, 1, 2), --1,7
+	(115, 1, 2), --1,8
+	(116, 1, 2), --1,9
+	(117, 1, 2), --1,10
+	(101, 2, 2), --2,1
+	(102, 2, 2), --2,2
+	(103, 2, 2), --2,3
+	(104, 2, 2), --2,4
+	(118, 2, 2), --2,5
+	(119, 2, 2), --2,6
+	(120, 2, 2), --2,7
+	(100, 2, 2), --2,8
+	(109, 2, 2), --2,9
+	(110, 2, 2), --2,10
+	(105, 3, 2), --3,1
+	(106, 3, 2), --3,2
+	(107, 3, 2), --3,3
+	(108, 3, 2), --3,4
+	(119, 3, 2), --3,5
+	(120, 3, 2), --3,6
+	(100, 3, 2), --3,7
+	(102, 3, 2), --3,8
+	(103, 3, 2), --3,9
+	(104, 3, 2), --3,10
+	(105, 4, 2), --4,1
+	(106, 4, 2), --4,2
+	(107, 4, 2), --4,3
+	(108, 4, 2), --4,4
+	(101, 4, 2), --4,5
+	(102, 4, 2), --4,6
+	(109, 4, 2), --4,7
+	(110, 4, 2), --4,8
+	(111, 4, 2), --4,9
+	(112, 4, 2) --4,10
 ;
 
 SELECT * FROM book_Copies
@@ -175,83 +179,83 @@ SELECT * FROM book_Copies
 INSERT INTO book_Loans
 	(loans_book_id, loans_branch_id, loans_card_no, date_out, date_due)-- (book_id, branch-id, borrower card_no, when they got it, when it was/is due)
 	VALUES
-	(101, 1, 1000, '3/05/2019', '3/19/2019'),--1 Number of books out in the past 2 months
-	(102, 2, 1003, '2/01/2019', '2/15/2019'),--2
-	(103, 2, 1003, '2/15/2019', '3/01/2019'),--3
-	(104, 2, 1003, '2/17/2019', '3/03/2019'),--4
-	(105, 2, 1003, '3/03/2019', '3/16/2019'),--5
-	(112, 1, 1003, '3/03/2019', '3/16/2019'),--6
-	(113, 1, 1003, '3/04/2019', '3/17/2019'),--7
-	(106, 4, 1007, '2/01/2019', '2/15/2019'),--8
-	(107, 4, 1007, '2/15/2019', '3/01/2019'),--9
-	(108, 4, 1007, '3/01/2019', '3/14/2019'),--10
-	(109, 4, 1007, '3/14/2019', '3/28/2019'),--11
-	(110, 4, 1007, '3/14/2019', '3/28/2019'),--12
-	(111, 4, 1007, '3/15/2019', '3/29/2019'),--13
-	(116, 1, 1002, '3/04/2019', '3/17/2019'),--14
-	(120, 3, 1004, '3/01/2019', '3/14/2019'),--15
-	(120, 3, 1005, '3/03/2019', '3/16/2019'),--16
-	(121, 3, 1004, '3/16/2019', '3/30/2019'),--17
-	(121, 3, 1005, '3/16/2019', '3/30/2019'),--18
-	(101, 1, 1006, '3/02/2019', '3/15/2019'),--19
-	(102, 2, 1001, '2/01/2019', '2/15/2019'),--20
-	(103, 2, 1001, '2/15/2019', '3/01/2019'),--21
-	(104, 2, 1001, '2/17/2019', '3/03/2019'),--22
-	(105, 2, 1001, '3/03/2019', '3/16/2019'),--23
-	(112, 1, 1001, '3/03/2019', '3/16/2019'),--24
-	(113, 1, 1001, '3/04/2019', '3/17/2019'),--25
-	(106, 4, 1002, '2/01/2019', '2/15/2019'),--26
-	(107, 4, 1002, '2/15/2019', '3/01/2019'),--27
-	(108, 4, 1002, '3/01/2019', '3/14/2019'),--28
-	(109, 4, 1002, '3/14/2019', '3/28/2019'),--29
-	(110, 4, 1002, '3/14/2019', '3/28/2019'),--30
-	(111, 4, 1002, '3/15/2019', '3/29/2019'),--31
-	(101, 1, 1004, '3/05/2019', '3/18/2019'),--32
-	(106, 3, 1005, '2/01/2019', '2/15/2019'),--33
-	(107, 3, 1005, '2/15/2019', '3/01/2019'),--34
-	(108, 3, 1005, '3/01/2019', '3/14/2019'),--35
-	(109, 3, 1005, '3/14/2019', '3/28/2019'),--36
-	(110, 3, 1005, '3/14/2019', '3/28/2019'),--37
-	(111, 3, 1005, '3/15/2019', '3/29/2019'),--38
-	(106, 3, 1000, '2/01/2019', '2/15/2019'),--39
-	(107, 3, 1000, '2/15/2019', '3/01/2019'),--40
-	(108, 3, 1000, '3/01/2019', '3/14/2019'),--41
-	(109, 3, 1000, '3/14/2019', '3/28/2019'),--42
-	(110, 3, 1000, '3/14/2019', '3/28/2019'),--43
-	(111, 3, 1000, '3/15/2019', '3/29/2019'),--44
-	(120, 2, 1007, '3/01/2019', '3/14/2019'),--45
-	(121, 2, 1007, '3/14/2019', '3/27/2019'),--46
-	(120, 2, 1001, '3/01/2019', '3/14/2019'),--47
-	(121, 2, 1001, '3/14/2019', '3/27/2019'),--48
-	(110, 1, 1001, '3/02/2019', '3/15/2019'),--49
-	(111, 1, 1001, '3/15/2019', '3/28/2019')--50
+	(100, 1, 1000, '3/05/2019', '3/19/2019'),--1 Number of books out in the past 2 months
+	(101, 2, 1003, '2/01/2019', '2/15/2019'),--2
+	(102, 2, 1003, '2/15/2019', '3/01/2019'),--3
+	(103, 2, 1003, '2/17/2019', '3/03/2019'),--4
+	(104, 2, 1003, '3/03/2019', '3/16/2019'),--5
+	(111, 1, 1003, '3/03/2019', '3/16/2019'),--6
+	(112, 1, 1003, '3/04/2019', '3/17/2019'),--7
+	(105, 4, 1007, '2/01/2019', '2/15/2019'),--8
+	(106, 4, 1007, '2/15/2019', '3/01/2019'),--9
+	(107, 4, 1007, '3/01/2019', '3/14/2019'),--10
+	(108, 4, 1007, '3/14/2019', '3/28/2019'),--11
+	(109, 4, 1007, '3/14/2019', '3/28/2019'),--12
+	(110, 4, 1007, '3/15/2019', '3/29/2019'),--13
+	(115, 1, 1002, '3/04/2019', '3/17/2019'),--14
+	(119, 3, 1003, '3/01/2019', '3/14/2019'),--15
+	(119, 3, 1005, '3/03/2019', '3/16/2019'),--16
+	(120, 3, 1006, '3/16/2019', '3/30/2019'),--17
+	(120, 3, 1005, '3/16/2019', '3/30/2019'),--18
+	(100, 1, 1006, '3/02/2019', '3/15/2019'),--19
+	(101, 2, 1001, '2/01/2019', '2/15/2019'),--20
+	(102, 2, 1001, '2/15/2019', '3/01/2019'),--21
+	(103, 2, 1001, '2/17/2019', '3/03/2019'),--22
+	(104, 2, 1001, '3/03/2019', '3/16/2019'),--23
+	(111, 1, 1001, '3/03/2019', '3/16/2019'),--24
+	(112, 1, 1001, '3/04/2019', '3/17/2019'),--25
+	(105, 4, 1002, '2/01/2019', '2/15/2019'),--26
+	(106, 4, 1002, '2/15/2019', '3/01/2019'),--27
+	(107, 4, 1002, '3/01/2019', '3/14/2019'),--28
+	(108, 4, 1002, '3/14/2019', '3/28/2019'),--29
+	(109, 4, 1002, '3/14/2019', '3/28/2019'),--30
+	(110, 4, 1002, '3/15/2019', '3/29/2019'),--31
+	(100, 1, 1001, '3/05/2019', '3/18/2019'),--32
+	(105, 3, 1005, '2/01/2019', '2/15/2019'),--33
+	(106, 3, 1005, '2/15/2019', '3/01/2019'),--34
+	(107, 3, 1005, '3/01/2019', '3/14/2019'),--35
+	(108, 3, 1005, '3/14/2019', '3/28/2019'),--36
+	(109, 3, 1004, '3/14/2019', '3/28/2019'),--37
+	(110, 3, 1005, '3/15/2019', '3/29/2019'),--38
+	(105, 3, 1000, '2/01/2019', '2/15/2019'),--39
+	(106, 3, 1000, '2/15/2019', '3/01/2019'),--40
+	(107, 3, 1000, '3/01/2019', '3/14/2019'),--41
+	(108, 3, 1000, '3/14/2019', '3/28/2019'),--42
+	(109, 3, 1000, '3/14/2019', '3/28/2019'),--43
+	(110, 3, 1000, '3/15/2019', '3/29/2019'),--44
+	(119, 2, 1007, '3/01/2019', '3/14/2019'),--45
+	(120, 2, 1007, '3/14/2019', '3/27/2019'),--46
+	(119, 2, 1001, '3/01/2019', '3/14/2019'),--47
+	(120, 2, 1001, '3/14/2019', '3/27/2019'),--48
+	(109, 1, 1001, '3/02/2019', '3/15/2019'),--49
+	(110, 1, 1001, '3/15/2019', '3/28/2019')--50
 ;
 SELECT * FROM book_Loans
 
 INSERT INTO book_Authors
 	(author_book_id, author_name)
 	VALUES
-	(101, 'Mark W. Lee'),
+	(100, 'Mark W. Lee'),
+	(101, 'Stephen King'),
 	(102, 'Stephen King'),
 	(103, 'Stephen King'),
 	(104, 'Stephen King'),
-	(105, 'Stephen King'),
+	(105, 'Clever Writer'),
 	(106, 'Clever Writer'),
 	(107, 'Clever Writer'),
 	(108, 'Clever Writer'),
-	(109, 'Clever Writer'),
-	(110, 'Jeff Kinney'),
-	(111, 'Rick Riordan'),
-	(112, 'J.R.R. Tolkien'),
-	(113, 'William Golding'),
-	(114, 'Suzanne Collins'),
+	(109, 'Jeff Kinney'),
+	(110, 'Rick Riordan'),
+	(111, 'J.R.R. Tolkien'),
+	(112, 'William Golding'),
+	(113, 'Suzanne Collins'),
+	(114, 'J.K. Rowling'),
 	(115, 'J.K. Rowling'),
 	(116, 'J.K. Rowling'),
 	(117, 'J.K. Rowling'),
 	(118, 'J.K. Rowling'),
-	(119, 'J.K. Rowling'),
-	(120, 'C.S. Lewis'),
-	(121, 'C.S. Lewis')
+	(119, 'C.S. Lewis'),
+	(120, 'C.S. Lewis')
 ;
 SELECT * FROM book_Authors
 
@@ -276,9 +280,7 @@ SELECT -- Query #3 Get all names of borrowers who do not have any books checked 
 	a1.borrower_name, a2.loans_card_no
 	FROM book_Loans a2
 	INNER JOIN borrower a1 ON a1.card_no = a2.loans_card_no
-	WHERE loans_card_no IN
-	(SELECT loans_card_no FROM book_Loans
-	GROUP BY loans_card_no HAVING COUNT (loans_card_no) = 0)
+	WHERE borrower_name IS NULL
 ; 
 
 
@@ -294,22 +296,27 @@ SELECT -- Query #4 Each book that is due today (3/19/2019 when this was written)
 ;
 
 SELECT -- Query #5 Get total number of books loaned from each branch
-	a1.branch_name, a2.loans_branch_id, a3.book_id, a4.number_of_copies
+	a1.branch_name, a2.loans_branch_id, a3.copies_book_id, a4.book_title , COUNT(DISTINCT loans_book_id) 
 	FROM book_Loans a2
-	INNER JOIN books a3 ON a3.book_id = a2.loans_book_id
-	INNER JOIN book_Copies a4 ON a4.copies_book_id = a3.book_id
 	INNER JOIN library_Branch a1 ON a1.branch_id = a2.loans_branch_id
+	INNER JOIN book_Copies a3 ON a3.copies_branch_id = a1.branch_id
+	INNER JOIN books a4 ON a4.book_id = a3.copies_book_id
+	WHERE branch_id IN
+	(SELECT branch_id FROM library_Branch
+	GROUP BY branch_id HAVING COUNT (branch_id) < 5)
+	GROUP BY  a1.branch_name, a2.loans_branch_id, a3.copies_book_id, a4.book_title
 ;
 
 
 SELECT -- Query #6 Get names and addresses of everyone who has more that 5 books checked out
-	a1.borrower_name, a1.borrower_address, a2.number_of_copies, a3.loans_card_no
+	a1.borrower_name, a1.borrower_address, a2.number_of_copies, a3.loans_card_no, COUNT (loans_card_no) 
 	FROM book_Copies a2
 	INNER JOIN book_Loans a3 ON a3.loans_branch_id = a2.copies_branch_id
 	INNER JOIN borrower a1 ON a1.card_no = a3.loans_card_no
 	WHERE loans_card_no IN
 	(SELECT loans_card_no FROM book_Loans
 	GROUP BY loans_card_no HAVING COUNT (loans_card_no) > 5)
+	GROUP BY a1.borrower_name, a1.borrower_address, a2.number_of_copies, a3.loans_card_no
 ; 
 
 SELECT --Query #7 Get number of copies of all books by Stephen King in the Central branch
